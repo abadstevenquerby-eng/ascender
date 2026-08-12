db748cd9959987de031da65c79428661990ead11
abad.stevenquerby@ue.edu.ph <abad.stevenquerby@ue.edu.ph>
2026-08-12 21:51:13 +0800

    Revert "finalized jump"
    This reverts commit 12a19a22a9a843d7d8bd60f449917a03b025c153.

--- script/player.gd
+++ script/player.gd
@@ -2,9 +2,6 @@ extends CharacterBody2D
 
 #The variable accessing the animations in animatedsprite2d
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var wall_slide: RayCast2D = %wall_slide
-
-
 
 #Games states
 enum State{
@@ -12,7 +9,6 @@ enum State{
 	Floor,
 	Jump,
 	Wall_Jump,
-	Slide,
 	Swing
 }
 
@@ -22,18 +18,7 @@ const fall_velocity = 500.0
 const walk_velocity = 200.0
 const jump_velocity = -600.0
 const jump_deceleration = 1500.0
-const increment = 250 #multiplier for jump force
-const max_y = -1000.0 #maximum vertical jump
-const max_x = 1000.0 #maximum horizontal jump
-const wall_gravity = 300.0 #Used to calculate how fast the player slides
-const wall_velocity = 500.0 #Maximum slide speed
-var current_x:float = 0.0 #initial variable for jumping horizontally
-var can_move = true #variable for when character can move or not
-var flipped = false #variable for changing the horizontal value when changing directions
-var current_y:float =  0.0 #Initial jump force
-
-
-
+var jump_x = 0
 
 #Default state is fall for now, might change it to floor one day
 var active_state := State.Fall
@@ -42,29 +27,15 @@ var active_state := State.Fall
 func _ready() -> void:
 	switch_state(active_state)
 
-
-#Is used to process physics inside the different states
+func hold():
+		animated_sprite_2d.animation = "hold"
+	
+		
+	
 func _physics_process(delta: float) -> void:
 	process_state(delta)
 	move_and_slide()
-	if Input.is_action_pressed("jump") and animated_sprite_2d.animation == "idle":
-		can_move = false #prohibits movement while charging jump
-		if flipped == false: #if flipped uses negative horizontal values otherwise use positive values for default
-			current_x = move_toward(current_x, max_x , increment *delta)
-		else:
-			current_x= move_toward(current_x, max_y, increment * delta)
-		current_y= move_toward(current_y, max_y, increment * delta)
-		animated_sprite_2d.animation = "hold"
 
-#Used because states change the value for everyframe and will get stuck if this part is inside the state
-func jump_process() -> void:
-		current_y = 0
-		if flipped == true:
-			current_x = -1
-		else:
-			current_x = 1
-		
-	
 #Used to switch and match states
 func switch_state(to_state: State) -> void:
 	active_state = to_state
@@ -75,10 +46,8 @@ func switch_state(to_state: State) -> void:
 	
 		State.Jump:
 			animated_sprite_2d.animation = "jump"
-		
-		State.Slide:
-			animated_sprite_2d.animation = "wall climb"
-			velocity.y = 0
+			velocity.y = jump_velocity
+
 
 
 #Defines what each states does.
@@ -86,56 +55,38 @@ func process_state(delta: float) -> void:
 	match active_state:
 		State.Fall:
 			velocity.y = move_toward(velocity.y, fall_velocity, fall_gravity * delta)
+			velocity.x = 0
 			if is_on_floor():
 				switch_state(State.Floor)
-			elif can_slide():
-				print("can slide")
-				switch_state(State.Slide)
-			
+				
 		State.Floor:
-			if Input.get_axis("left", "right") and can_move == true:
+			if Input.get_axis("left", "right"):
 				animated_sprite_2d.animation = "run"
 			else:
 				animated_sprite_2d.animation = "idle"
-			if can_move == true:
-				movement()
+			movement()
 			if not is_on_floor():
 				switch_state(State.Fall)
+			elif Input.is_action_pressed("jump"):
+				hold()
 			elif Input.is_action_just_released("jump"):
-				can_move = true
-				switch_state(State.Jump)
-
-		State.Jump:
-			velocity.x = current_x
-			velocity.y = current_y
-			jump_process()
-			if velocity.y <= 0 or velocity.y >= 0:
-				switch_state(State.Fall)
+					switch_state(State.Jump)
 #In progress
-		State.Slide:
-			velocity.y = move_toward(velocity.y, wall_velocity, wall_gravity * delta)
-			movement()
-			if is_on_floor():
-				switch_state(State.Floor)
-			elif not can_slide():
+		State.Jump:
+			velocity.x = 0
+			velocity.y = move_toward(velocity.y, 0, jump_deceleration * delta )
+			velocity.x = move_toward(velocity.x, jump_x, jump_deceleration * delta)
+			if Input.is_action_just_released("jump") or velocity.y >= 0:
+				jump_x = 100
 				switch_state(State.Fall)
-
 
 #Defines movement and direction of jumps 
 func movement() -> void:
-	var direction := (Input.get_axis("left","right"))
+	var direction := Input.get_axis("left","right")
 	if direction:
 		animated_sprite_2d.flip_h = direction < 0
-		#Forces the raycast to change directions
-		wall_slide.position.x = direction * absf(wall_slide.position.x)
-		wall_slide.target_position.x = direction * absf(wall_slide.target_position.x)
-		wall_slide.force_raycast_update()
-	velocity.x = direction * walk_velocity #Allows walking
-	if animated_sprite_2d.flip_h == true: #changes the values used for jumping horizontally when the image gets flipped
-		flipped = true
+	velocity.x = direction * walk_velocity
+	if animated_sprite_2d.flip_h == true:
+		jump_x = -50
 	else:
-		flipped = false
-		
-
-func can_slide() -> bool: #used for wall sliding
-	return is_on_wall_only() and wall_slide.is_colliding()
+		jump_x = 50
