129127f22bf53d6bf34ecc1c8fcc69039c576ca7
abadstevenquerby-eng <abad.stevenquerby@ue.edu.ph>
2026-09-13 22:49:50 +0800

    lvl3

--- level_selection.gd
+++ level_selection.gd
@@ -20,7 +20,7 @@ func _on_level_1_pressed() -> void:
 
 
 func _on_level_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://levels/testlevel/testmushroom.tscn")
+	get_tree().change_scene_to_file("res://levels/level_3/level3.tscn")
 
 
 
